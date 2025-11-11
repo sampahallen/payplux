cd8f3707a6cf95563d02b62679e2486aa1aa04e5
sampahallen <sampahallen@gmail.com>
2025-11-11 14:55:33 +0000

    feat: Add gh-pages deployment script and update routing for base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pay-plux">
       <App />
     </BrowserRouter>
   </StrictMode>
