b96125ac6ebc59c9f7640af0452b5f4472ecfe7d
sampahallen <sampahallen@gmail.com>
2025-11-11 16:12:49 +0000

    fix: Update BrowserRouter basename from '/pay-plux' to '/payplux'

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/pay-plux">
+    <BrowserRouter basename="/payplux">
       <App />
     </BrowserRouter>
   </StrictMode>
