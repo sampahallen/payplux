d1542f086277d226acb1a7872d64ab813da4ab1c
sampahallen <sampahallen@gmail.com>
2025-11-12 11:12:34 +0000

    fix: Update minimum widths in Navbar and Ref components for improved responsiveness

--- src/components/home/Ref.tsx
+++ src/components/home/Ref.tsx
@@ -5,15 +5,15 @@ import { NavLink } from "react-router";
 
 const Ref = () => {
   return (
-    <section className="bg-gray-50 flex flex-col gap-40 items-center justify-center mt-45 lg:mt-20 py-20 lg:mb-30 xl:mb-10">
-      <div className="flex flex-col md:flex-row lg:flex-row-reverse items-center justify-center px-4 md:px-15 md:gap-5 lg:gap-0 min-w-[360px] md:min-w-[900px] lg:min-w-[800px] xl:min-w-[1100px]">
-        <div className="flex flex-col w-[50%] gap-5 md:gap-9  xl:pl-15">
+    <section className="bg-gray-50 flex flex-col gap-30 lg:gap-40 items-center justify-center mt-45 lg:mt-20 py-20 lg:mb-30 xl:mb-10 overflow-x-hidden">
+      <div className="flex flex-col md:flex-row lg:flex-row-reverse md:items-center md:justify-center px-4 md:px-15 md:gap-5 lg:gap-0 min-w-[480px] md:min-w-[800px] lg:min-w-[800px] xl:min-w-[1100px]">
+        <div className="flex flex-col md:w-[50%] gap-5 md:gap-9  xl:pl-15">
           <h1 className="text-[30px] md:text-[28px] lg:text-[35px] text-slate-900 leading-[40px] font-semibold lg:font-medium">
             Spread the good news <br />
-            and <span className="text-blue-500">make money</span> doing <br />
+            and <span className="text-blue-500">make money</span> <br className="md:flex lg:hidden"/>doing <br className="md:hidden lg:flex"/>
             so!
           </h1>
-          <p className="text-[17px] md:text-[20px] lg:max-w-[500px] xl:max-w-[550px] font-normal text-slate-500">
+          <p className="text-[17px] md:text-[20px] md:max-w-[400px] lg:max-w-[500px] xl:max-w-[550px] font-normal text-slate-500">
             Our Affiliate and Referral program here at PayPlux is created to
             reward you for being a great friend who shows your friends the great
             things you discover! Who doesn't want to be a good friend and get
@@ -23,15 +23,15 @@ const Ref = () => {
             Coming soon
           </p>
         </div>
-        <div className="w-[50%]">
-          <div className="lg:w-[400px] lg:h-[500px] xl:w-[550px] xl:h-[650px]">
+        <div className="md:w-[50%]">
+          <div className="md:w-[350px] lg:w-[400px] lg:h-[500px] xl:w-[550px] xl:h-[650px]">
             <img src={ref} alt="" className="mt-10 md:mt-0 w-full h-full" />
           </div>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row-reverse lg:flex-row md:items-center lg:justify-center px-4 mt-30 text-slate-950 md:px-15 md:gap-5 lg:gap-0 xl:gap-7 min-w-[360px] md:min-w-[900px] lg:min-w-[1000px] xl:min-w-[1300px]">
-        <div className="flex flex-col gap-4 w-[50%]">
-          <h1 className="text-[28px] text-[34px] font-medium ">
+      <div className="flex flex-col md:flex-row-reverse lg:flex-row md:items-center lg:justify-center px-4 lg:mt-30 text-slate-950 md:px-15 md:gap-5 lg:gap-0 xl:gap-7 min-w-[360px] md:min-w-[800px] lg:min-w-[1000px] xl:min-w-[1300px]">
+        <div className="flex flex-col gap-4 md:w-[50%]">
+          <h1 className="text-[30px] lg:text-[34px] font-medium ">
             Perform your first <br />
             transaction on <span className="text-blue-500">PayPlux</span> <br />
             easily
@@ -49,7 +49,7 @@ const Ref = () => {
           <NavLink
             to=""
             className={({ isActive }) =>
-              `bg-blue-500 text-white text-[15px] font-semibold rounded-lg text-center py-5 w-[300px] md:w-[380px] lg:w-[250px] ${
+              `bg-blue-500 text-white text-[15px] font-semibold rounded-lg text-center py-5 w-[380px] md:w-[360px] lg:w-[250px] ${
                 isActive ? "border-4 border-sky-200" : "border-0"
               }`
             }
@@ -57,8 +57,8 @@ const Ref = () => {
             Place First Order!
           </NavLink>
         </div>
-        <div className="w-[50%]">
-          <div className="lg:w-[450px] lg:h-[450px] xl:w-[550px] xl:h-[650px]">
+        <div className="md:w-[50%]">
+          <div className="md:w-[300px] lg:w-[450px] lg:h-[450px] xl:w-[550px] xl:h-[650px]">
             <img src={first} alt="" className="mt-10 md:mt-0" />
           </div>
         </div>
